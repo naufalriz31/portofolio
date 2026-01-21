54c068013584fa790098d23bc848318ac07c97b5
naufalriz31 <rizqynaufal31@gmail.com>
2026-01-21 14:08:35 +0700

    perbaikan

--- script.js
+++
@@ -1,9 +0,0 @@
-const toggle = document.getElementById("themeToggle");
-const body = document.body;
-
-toggle.addEventListener("click", () => {
-  body.classList.toggle("dark");
-  toggle.textContent = body.classList.contains("dark")
-    ? "☀️ Light Mode"
-    : "🌙 Dark Mode";
-});
